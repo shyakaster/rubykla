1531a6b75d5f12a9015f8d338056d86b317f2c19
alex shyaka <shyakaster@gmail.com>
2018-03-04 21:30:29 +0300

    Add blog design and upload functionality

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,13 @@ class Post < ApplicationRecord
 
   friendly_id :title, use: :slugged
 
+  scope :most_recent, -> { order(id: :desc)}
+
   def should_generate_new_friendly_id?
     title_changed?
+  end
+
+  def display_day_published
+    "Published: #{created_at.strftime('%-b %-d %Y')}"
   end
 end
