0fad61ce72d55366b369fe6048ccc423d9919c21
alex shyaka <shyakaster@gmail.com>
2018-03-07 17:28:39 +0300

    add text editor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,10 @@
 //= require turbolinks
 //= require_tree .
 
+document.addEventListener("turbolinks:load", function() {
+  tinymce.remove();
+  tinymce.init({ selector:'textarea#post_body',
+  plugins: "codesample image media link code",
+  toolbar: "undo redo | styleselect | bold italic link | codesample image media | code"
+   });
+})
