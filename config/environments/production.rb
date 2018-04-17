01cb2b8fbaa672306facd4a93720c035115a54b0
alex shyaka <shyakaster@gmail.com>
2018-04-17 16:55:22 +0300

    change url in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { host: 'http://rubykampala.com/'}
+  config.action_mailer.default_url_options = { host: 'http://www.rubykampala.com'}
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
