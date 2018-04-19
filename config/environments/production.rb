c1cf1283a8a083d4426c030d9ae23e0c66d23b02
alex shyaka <shyakaster@gmail.com>
2018-04-19 16:21:37 +0300

    remove sending email for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { host: 'http://www.rubykampala.com'}
+  # config.action_mailer.default_url_options = { host: 'http://www.rubykampala.com'}
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
@@ -101,6 +101,6 @@ Rails.application.configure do
 #    :authentication       => "plain"
 #   }
   config.action_mailer.delivery_method = :smtp
-
+  config.action_mailer.default_url_options = { host: 'rubykla.herokuapp.com',:protocol =>'https' }
 end
 
